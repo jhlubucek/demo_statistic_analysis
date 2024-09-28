665dc63243cc40989908b137491cb7f379e4c80d
jhlubucek <jan.hlubucek@principal.cz>
2024-09-28 15:29:00 +0200

    rector config

--- php_rektor/refactor.rector.php
+++ php_rektor/refactor.rector.php
@@ -6,10 +6,16 @@ use Rector\Config\RectorConfig;
 use Rector\Php71\Rector\List_\ListToArrayDestructRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 
-return RectorConfig::configure()
-    ->withPaths([__DIR__ . '/src/refactor'])
-    ->withImportNames(removeUnusedImports: true)
-    ->withRules([
-        ListToArrayDestructRector::class,
-        NullToStrictStringFuncCallArgRector::class,
+return static function (RectorConfig $rectorConfig): void {
+    // Define the paths to refactor
+    $rectorConfig->paths([
+        __DIR__ . '/src/refactor',
     ]);
+
+    // Option to remove unused imports
+    $rectorConfig->importNames();
+
+    // Define individual rules to apply
+    $rectorConfig->rule(ListToArrayDestructRector::class);
+    $rectorConfig->rule(NullToStrictStringFuncCallArgRector::class);
+};
