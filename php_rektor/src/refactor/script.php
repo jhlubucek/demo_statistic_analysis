18db1abc3963c405b1f85d27e716e1a6351de753
jhlubucek <jan.hlubucek@principal.cz>
2024-09-28 13:30:49 +0200

    rektor

--- php_rektor/src/refactor/script.php
+++ php_rektor/src/refactor/script.php
@@ -1,1 +1,25 @@
 <?php
+
+declare(strict_types=1);
+
+namespace Refactor;
+
+class Example
+{
+    public function listExample()
+    {
+        list($first, $second) = [1, 2]; // List syntax to be refactored
+        echo $first, ',', $second;
+    }
+
+    public function nullToStrictString(?string $input)
+    {
+        return strtoupper($input); // Null argument to be handled
+    }
+}
+
+// Example usage
+$example = new Example();
+$example->listExample();
+echo $example->nullToStrictString(null);
+echo $example->nullToStrictString('Hello');
